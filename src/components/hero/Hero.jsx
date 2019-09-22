958228f6ca175d6847d2fe84922aa473d8bf2f2b
DutraNeto <dudutraneto@gmail.com>
2019-09-21 21:16:58 -0400

    add background image

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import './Hero.scss'
+
+class Hero extends Component {
+  constructor(props) {
+    super(props)
+  }
+    render() {
+      return(
+        <div className="Hero">Hello</div>
+      )
+    }
+}
+
+export default Hero
