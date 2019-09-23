09bbb3af8dd7435d62b5f3fcb92f26f551ffbf4c
DutraNeto <dudutraneto@gmail.com>
2019-09-23 11:05:48 -0400

    add font styles and text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, {Fragment} from 'react';
-import Hero from './components/hero/Hero'
+import React, { Fragment } from 'react'
+import Header from './components/header/Header'
 
 function App() {
-  return (
-    <Fragment>
-      <Hero />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Header />
+		</Fragment>
+	)
 }
 
-export default App;
+export default App
