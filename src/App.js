bc39bfc02b14a687d6287e0da6985e648b59c70e
DutraNeto <dudutraneto@gmail.com>
2019-09-23 23:18:11 -0400

    add credits and styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react'
 import Header from './components/header/Header'
 import Main from './components/main/Main'
+import Footer from './components/footer/Footer'
 
 function App() {
 	return (
 		<Fragment>
 			<Header />
       <Main />
+			<Footer />
 		</Fragment>
 	)
 }
