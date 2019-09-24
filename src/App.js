6213258bd97543c934ea000fc2c907df688d5329
DutraNeto <dudutraneto@gmail.com>
2019-09-23 21:57:07 -0400

    add state to main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react'
 import Header from './components/header/Header'
+import Main from './components/main/Main'
 
 function App() {
 	return (
 		<Fragment>
 			<Header />
+      <Main />
 		</Fragment>
 	)
 }
